6d7a58573e3a4e02aa3ca02ea28d19f8519093b2
Kamila Martha <142358676+kamilamartha@users.noreply.github.com>
2023-10-22 23:31:17 -0300

    Add files via upload

---
+++ iphone-java basico/iphone-java-basico/README.md
@@ -0,0 +1,9 @@
++---------------------+
+|       iPhone       |
+|---------------------|
+| - modelo: String   |
+|---------------------|
+| + tocarMusica(): void |
+| + fazerChamada(numero: String): void |
+| + navegarInternet(url: String): void |
++---------------------+
---
+++ iphone-java basico/iphone-java-basico/src/iPhone.java
@@ -0,0 +1,20 @@
+public class iPhone {
+    private String modelo;
+
+    public iPhone(String modelo) 
+{
+        this.modelo = modelo;
+    }
+
+    public void tocarMusica() {
+       
+    }
+
+    public void fazerChamada(String numero) {
+        
+    }
+
+    public void navegarInternet(String url) {
+        
+    }
+}
